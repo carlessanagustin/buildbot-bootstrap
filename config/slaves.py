ca71da17beaa1d945f98b94e1c1e8cb57852e0a0
Carles San Agustin <carles@carlessanagustin.com>
2014-11-04 21:22:43 +0100

    for later: iterate slaves

--- config/slaves.py
+++ config/slaves.py
@@ -10,8 +10,19 @@
 from buildbot.buildslave import BuildSlave
 
 def get_slaves():
+	
 	slaves = []
 
 	slaves.append(BuildSlave('myslave', 'myslave-password'))
 	
 	return slaves
+
+"""
+def get_slaves(slaves_list):
+	slaves = []
+
+	for slave in slaves_list:
+		slaves.append(BuildSlave(slave))
+	
+	return slaves
+"""
